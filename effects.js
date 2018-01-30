9270ff5eb266d7c01605d48500d81c36bc3f7ca5
Kevin Hanley <117388451@umail.ucc.ie>
2018-01-30 19:37:32 +0000

    Adding content to CV

---
+++ effects.js
@@ -0,0 +1,9 @@
+      function fade() {
+      document.getElementById("demo").style.visibility = "hidden";
+      document.getElementById("replace").style.visibility = "visible";
+      }
+      
+      function hide() {
+      document.getElementById("replace").style.visibility = "hidden";
+      } 
+ 
