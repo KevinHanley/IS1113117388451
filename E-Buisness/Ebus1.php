6e25100566d71bdfe241573b2f2b6d32e227a280
Kevin Hanley <117388451@umail.ucc.ie>
2018-02-01 09:47:34 +0000

    Adding php pages

---
+++ E-Buisness/Ebus1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Select Product</title>
+        
+        <!-- JQuery -->
+        <script src="https//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="cost_calc.js"></script>
+    </head>
+    
+    <body>
+        
+        <h1>Select a Product</h1>
+        
+        <br>
+        
+        <form method="POST" action="Ebus2.php">
+            
+            <label for="salesforce">
+                <input type="radio" id="salesforce" name="product" checked onclick="disablebtnProceed()"/>
+                SalesForce @ $100
+            </label>
+            
+            <br>
+            
+            <label for="aws">
+                <input type="radio" id="aws" name="product" onclick="disablebtnProceed()"/>
+                aws @ $300
+            </label>
+            
+            <br>
+            <br>
+            
+            <label for="subtotal">
+                Sub Total
+                <input type="text" id="subtotal" value="0.00" readonly/>
+            </label>
+            
+            <br>
+            
+            <label for="total">
+                Total
+                <input type="text" id="total" value="0.00" readonly/>
+            </label>
+            
+            <br>
+            
+            <button type="submit" id="btnProceed" disabled >Add to Shipping Cart</button>
+            
+        </form>
+        
+        <br>
+        
+        <button onclick="calSub()">Calculate Cost</button>
+        <a role="button" href="Ebus1.php">Clear Choice</a>
+
+    </body>
+</html>
