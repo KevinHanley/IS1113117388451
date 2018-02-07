f7497f31c8e9712548a673663eceaeed89dcd35b
Kevin Hanley <117388451@umail.ucc.ie>
2018-02-07 20:48:48 +0000

    New Nav Bar

--- Homepage/effects.js
+++
@@ -1,9 +0,0 @@
-      function fade() {
-      document.getElementById("demo").style.visibility = "hidden";
-      document.getElementById("replace").style.visibility = "visible";
-      }
-      
-      function hide() {
-      document.getElementById("replace").style.visibility = "hidden";
-      } 
- 
